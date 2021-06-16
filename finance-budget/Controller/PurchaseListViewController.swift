a15aaca903b825b5b46ccc59e5b93ffbdd1171e2
djud17 <davidtonoyan10@rambler.ru>
2021-06-16 11:59:20 +0300

    update project

---
+++ finance-budget/Controller/PurchaseListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PurchaseListViewController.swift
+//  finance-budget
+//
+//  Created by Давид Тоноян  on 15.06.2021.
+//
+
+import UIKit
+
+class PurchaseListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
