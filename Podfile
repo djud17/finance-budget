a1bcd66beb5c0e330508f39d51ffbb375c699f04
David Tonoyan <davidtonoyan10@me.com>
2022-11-03 18:25:26 +0400

    Remove pods

--- Podfile
+++
@@ -1,7 +0,0 @@
-platform :ios, '10.0'
-use_frameworks!
-
-target 'finance-budget' do
-pod 'RealmSwift'
-pod 'Charts'
-end
--- Pods/Target Support Files/Pods-finance-budget/Pods-finance-budget-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_finance_budget : NSObject
-@end
-@implementation PodsDummy_Pods_finance_budget
-@end
